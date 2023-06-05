862e3b49fba39988bbf72a25b2315a9a3ca35171
unknown <buiminhnhan2k@gmail.com>
2023-06-05 23:07:46 +0700

    new

--- src/common/database.js
+++ src/common/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true);
 const db = () => {
-    mongoose.connect('mongodb://0.0.0.0:27017/tech_shop', {
+    mongoose.connect('mongodb+srv://buiminhnhan2k:ailopdiu1996@cluster0.qvaxeak.mongodb.net/', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false,
